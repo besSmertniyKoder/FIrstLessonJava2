f2224ce2620a87ece99e69cf32cb55f0857210f1
Roman Sukhachev <rsukhachev@bostonsd.ru>
2023-10-21 10:10:18 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,23 @@
+public class MainClass {
+    public static void main(String[] args) {
+        int a = 10;
+        char c = 'a';
+        boolean b = true;
+        float f = 10.5F;
+        String s = "heelo world";
+        String s1 = new String("test");
+        System.out.println(f);
+        int[] intArray = {1,4,6,7,8,9,5,7,8,5,9,5,9};
+        for (int i = 0;i < intArray.length ; i++){
+
+            System.out.print(intArray[i] + " ");
+
+        }
+
+
+
+
+    }
+
+
+}
